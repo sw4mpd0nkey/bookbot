1e4fed8b5eff1c3d38b826a6138443f06de059a1
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-11-13 19:41:51 -0500

    refactored code into two files, stats.py will handle analysis of the texts

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+from stats import word_count
+
+def get_book_text(filepath):
+    file_contents = ""
+    with open(filepath) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+def main():
+    contents = get_book_text("books/frankenstein.txt")
+    num_words = word_count(contents)
+    print(f"Found {num_words} total words")
+
+main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def word_count(text):
+    count = len(text.split())
+    return count
