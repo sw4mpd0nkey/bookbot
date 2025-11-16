2a0fa3dfb7e3ccfac8b4afad364d941616c10687
sw4mpd0nkey <sw4mpd0nkey@protonmail.com>
2025-11-16 14:48:51 -0500

    pulling book locagtion from args

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from stats import word_count, char_count, sortaable_data
-
+import sys
 
 def get_book_text(filepath):
     file_contents = ""
@@ -18,8 +18,16 @@ def print_report(loc,word_count, chars):
     for ch in chars:
         print(f"{ch["char"]}: {ch["num"]}")
 
+def print_help():
+    print("Usage: python3 main.py <path_to_book>")
+
 def main():
-    content_loc = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print_help()
+        sys.exit(1)
+    else:
+        content_loc = sys.argv[1]
+
     contents = get_book_text(content_loc)
     num_words = word_count(contents)
     num_chars = char_count(contents)
